26ea73c01506e237a8e47e6c0e4793e0e9edc5f0
leesh <qwqw8019@naver.com>
2024-03-08 17:33:50 +0900

    feat: resume 도메인 생성

---
+++ meetingProject/src/main/java/hongikclubfair/meetingproject/domain/resume/domain/Resume.java
@@ -0,0 +1,42 @@
+package hongikclubfair.meetingproject.domain.resume.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Resume {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 20)
+	private String instagramId;
+
+	@Column(nullable = false, length = 100)
+	private String introduction;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Gender targetGender;
+
+	public Resume(String instagramId, String introduction, Gender gender, Gender targetGender) {
+		this.instagramId = instagramId;
+		this.introduction = introduction;
+		this.gender = gender;
+		this.targetGender = targetGender;
+	}
+}
