40a2bc4b6e4e2b0197c4f2e51bf4b58f59ec9c13
leesh <qwqw8019@naver.com>
2024-03-08 17:35:33 +0900

    feat: 사람 정보 입력 기능 구현

--- meetingProject/src/main/java/hongikclubfair/meetingproject/domain/resume/controller/ResumeController.java
+++ meetingProject/src/main/java/hongikclubfair/meetingproject/domain/resume/controller/ResumeController.java
@@ -1,8 +1,11 @@
 package hongikclubfair.meetingproject.domain.resume.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import hongikclubfair.meetingproject.domain.resume.dto.request.PostResumeRequest;
 import hongikclubfair.meetingproject.domain.resume.service.ResumeService;
 import lombok.RequiredArgsConstructor;
 
@@ -12,5 +15,10 @@ import lombok.RequiredArgsConstructor;
 public class ResumeController {
 
 	private final ResumeService resumeService;
+
+	@PostMapping
+	public void postResume(@RequestBody PostResumeRequest request) {
+		resumeService.postResume(request);
+	}
 
 }
--- meetingProject/src/main/java/hongikclubfair/meetingproject/domain/resume/service/ResumeService.java
+++ meetingProject/src/main/java/hongikclubfair/meetingproject/domain/resume/service/ResumeService.java
@@ -2,6 +2,8 @@ package hongikclubfair.meetingproject.domain.resume.service;
 
 import org.springframework.stereotype.Service;
 
+import hongikclubfair.meetingproject.domain.resume.domain.Resume;
+import hongikclubfair.meetingproject.domain.resume.dto.request.PostResumeRequest;
 import hongikclubfair.meetingproject.domain.resume.repository.ResumeRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +12,9 @@ import lombok.RequiredArgsConstructor;
 public class ResumeService {
 	private final ResumeRepository resumeRepository;
 
+	public void postResume(PostResumeRequest request) {
+		Resume resume = request.toResume();
+
+		resumeRepository.save(resume);
+	}
 }
