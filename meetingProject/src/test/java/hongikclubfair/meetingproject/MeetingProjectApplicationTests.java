2ebe4f2312ab093fbaee1caca57f573d3b97be73
hl3470 <hl3470@naver.com>
2024-03-08 12:46:29 +0900

    init

---
+++ meetingProject/src/test/java/hongikclubfair/meetingproject/MeetingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hongikclubfair.meetingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
