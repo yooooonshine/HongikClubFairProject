ef6359f7828ffd6a6fdfbd444b02fe808ba9920f
leesh <qwqw8019@naver.com>
2024-03-08 16:11:28 +0900

    feat: 예외 반환 객체 생성 메소드 추가

--- meetingProject/src/main/java/hongikclubfair/meetingproject/common/dto/ErrorResponse.java
+++ meetingProject/src/main/java/hongikclubfair/meetingproject/common/dto/ErrorResponse.java
@@ -3,6 +3,7 @@ package hongikclubfair.meetingproject.common.dto;
 import java.time.LocalDateTime;
 
 import hongikclubfair.meetingproject.common.exception.BaseErrorCode;
+import hongikclubfair.meetingproject.common.exception.MeetingException;
 
 public record ErrorResponse(
 	LocalDateTime timeStamp,
@@ -12,6 +13,13 @@ public record ErrorResponse(
 	String reason,
 	String path
 ) {
+	public static ErrorResponse fromException(MeetingException exception, String path) {
+		ErrorReason reason = exception.getErrorReason();
+		LocalDateTime now = LocalDateTime.now();
+
+		return new ErrorResponse(now, false, reason.status(), reason.code(), reason.reason(), path);
+	}
+
 	public static ErrorResponse fromErrorCode(BaseErrorCode errorCode, String path) {
 		ErrorReason reason = errorCode.getErrorCode();
 		LocalDateTime now = LocalDateTime.now();
