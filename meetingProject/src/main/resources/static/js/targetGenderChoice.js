58d3e918ded0b958b8caed75d0a0ed7feddaec5b
hl3470 <hl3470@naver.com>
2024-03-10 02:50:31 +0900

    feat: targetGenderChoice 예외 처리

--- meetingProject/src/main/resources/static/js/targetGenderChoice.js
+++ meetingProject/src/main/resources/static/js/targetGenderChoice.js
@@ -22,6 +22,8 @@ function submitForm(targetGender) {
     const introduction = localStorage.getItem("introduction");
     const gender = localStorage.getItem("gender");
 
+    localStorage.clear();
+
     $.ajax({
         type: 'post',
         url: '/api/resume',
@@ -33,10 +35,22 @@ function submitForm(targetGender) {
         },
         dataType: 'JSON',
         success: function (data) {
-            window.location.assign("/people_choice");
+            if (data.status === 200) {
+                localStorage.setItem("memberId", data.id);
+                window.location.assign("/people_choice");
+            }
+            if (data.status === 400) {
+                alert("형식이 잘못되었습니다.");
+            } else if (data.status === 409) {
+                alert("이미 존재하는 인스타아이디입니다.");
+            } else if (data.status === 500) {
+                alert("서버가 작동하지 않습니다.");
+            } else {
+                alert("알 수 없는 에러입니다.");
+            }
         },
         error: function (jqrXHR, status, error) {
-            // 추후 예외에 맞게 작성
+            alert("알 수 없는 에러입니다.");
         },
     })
 }
