7c9c4ec93a14ec4b019a87707997ecffa5e75e83
hl3470 <hl3470@naver.com>
2024-03-11 00:54:30 +0900

    refactor: choiceGender메서드명 변경 및 예외 처리 수정
    choiceGender메서드를 choiceTargetGender로 변경하였고, 예외를 ajax의 error에서 처리하도록 변경하였다. 또한 ajax를 동기로 수정하였다.

--- meetingProject/src/main/resources/static/js/targetGenderChoice.js
+++ meetingProject/src/main/resources/static/js/targetGenderChoice.js
@@ -2,7 +2,7 @@ const male = "MALE";
 const female = "FEMALE";
 const any = "ANY";
 
-function choiceGender(gender) {
+function choiceTargetGender(gender) {
     const targetGender = makeGenderResponseValue(gender);
     submitForm(targetGender);
 }
@@ -20,7 +20,7 @@ function makeGenderResponseValue(gender) {
 function submitForm(targetGender) {
     const instagramId = localStorage.getItem("instagramId");
     const introduction = localStorage.getItem("introduction");
-    const gender = localStorage.getItem("gender");
+    const gender = makeGenderResponseValue(localStorage.getItem("gender"));
 
     if (instagramId === null || introduction === null || gender === null) {
         alert("기본정보를 입력해주세요.");
@@ -37,56 +37,53 @@ function submitForm(targetGender) {
             'gender': gender,
             'targetGender': targetGender
         },
+        async: false,
         dataType: 'JSON',
         success: function (data) {
-            if (data.status === 200) {
-                localStorage.setItem("memberId", data.data);
-                window.location.assign("/people_choice");
-            }
-            if (data.status === 400) {
+            localStorage.setItem("memberId", data.data["id"]);
+            window.location.assign("/people_choice");
+        },
+        error: function (request,status,error) {
+            if (request.status === 400) {
                 alert("형식이 잘못되었습니다.");
-            } else if (data.status === 409) {
+            } else if (request.status === 409) {
                 alert("이미 존재하는 인스타아이디입니다. 기존 회원정보로 진행됩니다.");
 
                 localStorage.setItem("memberId", requestId(instagramId));
                 window.location.assign("/people_choice");
-            } else if (data.status === 500) {
+            } else if (request.status === 500) {
                 alert("서버가 작동하지 않습니다.");
             } else {
-                alert("알 수 없는 예외입니다.");
+                alert(request.status + " 예외입니다.");
             }
-        },
-        error: function () {
-            alert("서버와의 연결이 되지 않습니다.");
         },
     });
 }
 
 function requestId(instagramId) {
-    let memberId = 0;
+    var memberId = 0;
 
     $.ajax({
         type: 'get',
-        url: '/api/member/instagramId/' + instagramId,
+        url: '/api/resume/instagramId/' + instagramId,
         dataType: 'html',
+        async: false,
         success: function (data) {
-
-            if (data.status === 200) {
-                memberId = data.data
-            } else if (data.status === 400) {
+            memberId = data.data["id"];
+            // memberId = data.data
+        },
+        error: function (request,status,error) {
+            if (request.status === 400) {
                 alert("요청이 잘못되었습니다.");
-            } else if (data.status === 404) {
-                alert("존재하지 않는 인스타아이디입니다.")
-            } else if (data.status === 500) {
+            } else if (request.status === 404) {
+                alert("서버에 존재하지 않는 인스타아이디입니다.")
+            } else if (request.status === 500) {
                 alert("서버가 작동하지 않습니다.");
             } else {
-                alert("알 수 없는 예외입니다.");
+                alert(request.status + " 예외입니다.");
             }
         },
-        error: function (jqrXHR, status, error) {
-            alert("알 수 없는 에러입니다.");
-        },
-    })
+    });
 
     return memberId;
 }
