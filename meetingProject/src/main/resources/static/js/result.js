75b7b53a07b1e305f68b3ecdfc27937122aa2e66
hl3470 <hl3470@naver.com>
2024-03-13 02:10:44 +0900

    refactor: 약관동의확인 메서드 구현 및 백엔드 json에 맞게 변경

--- meetingProject/src/main/resources/static/js/result.js
+++ meetingProject/src/main/resources/static/js/result.js
@@ -1,13 +1,43 @@
 window.onload = function() {
+
+    checkTermsOfUse();
+
     const chosenMemberId = localStorage.getItem("chosenMemberId");
-    fetch("/api/resume/"+chosenMemberId)
-        .then((response) => response.json())
-        .then((result) => {
+    checkChosenMemberId(chosenMemberId);
+
+    requestChosenMember(chosenMemberId);
+}
+
+function checkChosenMemberId(chosenMemberId) {
+    if (chosenMemberId === null) {
+        alert("기본정보를 입력해주세요.");
+        window.location.assign("/member_form");
+    }
+}
+
+function checkTermsOfUse() {
+    const termChecked1 = localStorage.getItem("termChecked1");
+    const termChecked2 = localStorage.getItem("termChecked2");
+
+    if(!termChecked1 && !termChecked2){
+        alert("이용약관, 개인정보 수집/이용 동의서에 동의해주세요");
+        localStorage.removeItem("termChecked1");
+        localStorage.removeItem("termChecked2");
+        window.location.assign("/terms_of_use");
+    }
+}
+
+function requestChosenMember(chosenMemberId) {
+    $.ajax({
+        type: 'get',
+        url: '/api/resume/' + chosenMemberId,
+        async: false,
+        dataType: 'JSON',
+        success: function (result) {
             makeMemberCard(result.data.instagramId, result.data.introduction);
-        })
-        .catch((error) => {
+        },
+        error: function (request, status, error) {
             if (error.status === 400) {
-                alert("잘못된 요청입니다.");
             } else if(error.status === 404){
                 alert("선택된 사람을 찾지 못했습니다.");
                 window.location.assign("/people_choice");
@@ -16,7 +46,8 @@ window.onload = function() {
             } else {
                 alert("알 수 없는 예외입니다.");
             }
-        })
+        },
+    });
 }
 
 function makeMemberCard(instagramId, introduction) {
@@ -54,13 +85,16 @@ function submitForm(phoneNumber) {
 
     phoneNumber = phoneNumber.replace("-", "");
 
+    let data = {
+            'phoneNumber': phoneNumber,
+    };
+
     $.ajax({
         type: 'post',
         url: '/api/resume/send/' + memberId,
-        data: {
-            'phoneNumber': phoneNumber,
-        },
+        data: JSON.stringify(data),
         dataType: 'JSON',
+        contentType : "application/json",
         success: function (data) {
             window.localStorage.clear();
             window.location.assign("/");
@@ -73,7 +107,7 @@ function submitForm(phoneNumber) {
             } else if(request.status === 500){
                 alert("서버가 작동하지 않습니다.");
             } else {
-                alert("알 수 없는 예외입니다.");
+                alert(request.status + " 예외입니다.");
             }
         },
     })
