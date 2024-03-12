172d1d7bcab650bcb763b9e1d8d7db90abf2b450
leesh <qwqw8019@naver.com>
2024-03-12 23:25:47 +0900

    feat: redis 캐시 설정

--- meetingProject/src/main/java/hongikclubfair/meetingproject/common/config/RedisConfig.java
+++ meetingProject/src/main/java/hongikclubfair/meetingproject/common/config/RedisConfig.java
@@ -5,16 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
 
 	private final RedisProperties redisProperties;
@@ -30,7 +28,7 @@ public class RedisConfig {
 
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
 		return redisTemplate;
 	}
