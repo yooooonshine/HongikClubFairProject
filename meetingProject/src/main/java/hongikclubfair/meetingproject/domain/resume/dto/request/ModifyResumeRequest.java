86511b7657bbd5284e6d58d2075b620d2dcbd4c7
leesh <qwqw8019@naver.com>
2024-03-13 13:07:06 +0900

    fix: 성별, 타겟 성별 수정하도록 변경

--- meetingProject/src/main/java/hongikclubfair/meetingproject/domain/resume/dto/request/ModifyResumeRequest.java
+++ meetingProject/src/main/java/hongikclubfair/meetingproject/domain/resume/dto/request/ModifyResumeRequest.java
@@ -1,7 +1,11 @@
 package hongikclubfair.meetingproject.domain.resume.dto.request;
 
+import hongikclubfair.meetingproject.domain.resume.domain.Gender;
+
 public record ModifyResumeRequest(
 	String instagramId,
-	String introduction
+	String introduction,
+	Gender gender,
+	Gender targetGender
 ) {
 }
